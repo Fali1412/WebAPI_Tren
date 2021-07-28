2ca6b8f41b306b00ca10387a566ffd2b0d8a95d3
Fali1412 <fattev.ilya1337@gmail.com>
2021-07-28 21:53:40 +0700

    Add Health check

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -33,11 +37,11 @@ namespace WebAPI_Tren
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var MongoSettings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
             
             services.AddSingleton<IMongoClient>(serviceProvider =>
             {
-                var settings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(MongoSettings.ConnectionString);
             });
             
             services.AddSingleton<IInMemItemsRepository, MongoDBItemsRepository>();
@@ -50,6 +54,12 @@ namespace WebAPI_Tren
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI_Tren", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                .AddMongoDb(MongoSettings.ConnectionString,
+                    name: "MongoDB",
+                    timeout: TimeSpan.FromSeconds(3),
+                    tags: new[] {"ready"});
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -70,6 +80,32 @@ namespace WebAPI_Tren
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(new
+                        {
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(entry => new
+                            {
+                                name = entry.Key,
+                                status = entry.Value.Status.ToString(),
+                                exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                duration = entry.Value.Duration.ToString()
+                            })
+                            
+                        });
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
